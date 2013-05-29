3e361f9cd7d0fe650182954605f9374506873535
Ian Obermiller <iano@fb.com>
2013-05-28 22:33:32 -0700

    Send pongs.
    Send pong.
    Listen for connections.
    Fix error handling.

--- src/main.go
+++ src/main.go
@@ -18,47 +18,113 @@ const (
 	Reply             = 3
 )
 
-func main() {
-
-	messageId, err := genId()
-	if err != nil {
-		fmt.Printf("Error generating message id:", err)
-	}
-
-	b, err := buildMessage(messageId, Ping, 100, 0, []byte{})
-	if err != nil {
-		return
-	}
-
-	conn, err := net.Dial("tcp", "128.208.2.88:5002")
-	if err != nil {
-		fmt.Printf("Dial err:", err)
-		return
-	}
-
-	n, err := conn.Write(b)
-	if n != len(b) || err != nil {
-		fmt.Printf("Err writing to conn: ", err)
-		return
-	}
-
-	for {
-		buf := make([]byte, 2048)
-		n, err := conn.Read(buf)
-		if n == 0 {
-			continue
-		}
-		if err != nil {
-			fmt.Printf("ReadFull err:", err)
-		}
-
-		fmt.Printf("% x\n", buf[:n])
+func (t MessageType) String() string {
+	switch t {
+	case Ping:
+		return "PING "
+	case Pong:
+		return "PONG "
+	case Query:
+		return "QUERY"
+	case Reply:
+		return "REPLY"
+	default:
+		return "NONE "
 	}
 }
 
-func genId() (messageId []byte, err error) {
+const HOST = "128.208.1.137"
+const PORT int16 = 25610
+
+func main() {
+	host := "128.208.2.88:5002"
+
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		fmt.Println("Dial err:", err)
+		return
+	}
+
+	go handleConnection(conn)
+
+	go ping(conn)
+	startListener()
+}
+
+func ping(c net.Conn) {
+	send(c, Ping, genId(), []byte{})
+}
+
+func pong(c net.Conn, pingMessageId []byte) {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, PORT)
+	buf.Write(net.ParseIP(HOST).To4())
+	send(c, Pong, pingMessageId, buf.Bytes())
+}
+
+func send(c net.Conn, t MessageType, messageId []byte, payload []byte) {
+	b, err := buildMessage(messageId, t, 10, 0, payload)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("SEND %s %s - % x\n", t.String(), c.RemoteAddr(), b)
+
+	sendRaw(c, b)
+}
+
+func sendRaw(c net.Conn, b []byte) {
+	n, err := c.Write(b)
+	if n != len(b) || err != nil {
+		fmt.Println("Err writing to conn: ", err)
+		return
+	}
+}
+
+func startListener() {
+	ln, err := net.Listen("tcp", fmt.Sprint(":", PORT))
+	if err != nil {
+		fmt.Println("Listen error: ", err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Accept error: ", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	for {
+		buf := make([]byte, 2048)
+		n, err := c.Read(buf)
+
+		if err != nil {
+			fmt.Println("Read err:", err)
+		}
+
+		if n == 0 {
+			continue
+		}
+
+		t := MessageType(buf[16])
+		fmt.Printf("RECV %s %s - % x\n", t, c.RemoteAddr(), buf[:n])
+
+		switch t {
+		case Ping:
+			go pong(c, buf[:16])
+		}
+	}
+}
+
+func genId() (messageId []byte) {
 	messageId = make([]byte, 16)
-	_, err = io.ReadFull(rand.Reader, messageId)
+	_, err := io.ReadFull(rand.Reader, messageId)
+	if err != nil {
+		fmt.Errorf("ReadFull from rand err: %s\n", err)
+	}
 	return
 }
 
@@ -74,41 +140,39 @@ func buildMessage(
 
 	n, err := buf.Write(messageId)
 	if n != len(messageId) || err != nil {
-		fmt.Printf("Err:", err)
+		fmt.Println("Err:", err)
 		return
 	}
 
 	err = buf.WriteByte(byte(messageType))
 	if err != nil {
-		fmt.Printf("Err:", err)
+		fmt.Println("Err:", err)
 		return
 	}
 
 	err = buf.WriteByte(ttl)
 	if err != nil {
-		fmt.Printf("Err:", err)
+		fmt.Println("Err:", err)
 		return
 	}
 
 	err = buf.WriteByte(hops)
 	if err != nil {
-		fmt.Printf("Err:", err)
+		fmt.Println("Err:", err)
 		return
 	}
 
 	err = binary.Write(buf, binary.BigEndian, int32(len(payload)))
 	if err != nil {
-		fmt.Printf("Err:", err)
+		fmt.Println("Err:", err)
 		return
 	}
 
 	n, err = buf.Write(payload)
 	if n != len(payload) || err != nil {
-		fmt.Printf("Err:", err)
+		fmt.Println("Err:", err)
 		return
 	}
-
-	fmt.Printf("% x\n", buf.Bytes())
 
 	b = buf.Bytes()
 
