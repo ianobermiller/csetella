42ea84e2f6ed74a72b5483dc54325dfeb4d41f49
Ian Obermiller <iano@fb.com>
2013-06-04 22:16:42 -0700

    Send queries, cache message ids.

--- src/main.go
+++ src/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"./cache"
 	"bytes"
 	"crypto/rand"
 	"encoding/binary"
 	"flag"
 	"fmt"
 	"io"
+	mrand "math/rand"
 	"net"
 	"os"
+	"time"
 )
 
 type MessageType byte
@@ -35,10 +38,15 @@ func (t MessageType) String() string {
 	}
 }
 
+type Conn struct {
+	conn      *net.Conn
+	gotSecret bool
+}
+
 const KNOWN_ADDRESS = "128.208.2.88:5002"
 const HOST = "128.208.1.137"
 const PORT = 25610
-const SECRET = "Ian Obermiller <itao@uw.edu>"
+const SECRET = "Ian Obermiller -- iano [at] cs.washington.edu -- 25610"
 const LOG_FILE = "log.txt"
 
 var host string
@@ -47,6 +55,17 @@ var secret string
 var knownAddress string
 var logFile string
 
+var peers = make([]string, 0)
+var connections = make(map[string]*Conn)
+var gotSecret = make(map[string]bool)
+var msgCache = cache.New(5*time.Minute, 30*time.Second)
+
+func Usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\nOptions:\n")
+	flag.PrintDefaults()
+}
+
 func main() {
 	flag.StringVar(&host, "host", HOST, "external ip to send in pongs")
 	flag.IntVar(&port, "port", PORT, "external port to send in pongs")
@@ -54,8 +73,37 @@ func main() {
 	flag.StringVar(&knownAddress, "seed", KNOWN_ADDRESS, "initial seed node to ping (format host:port)")
 	flag.StringVar(&logFile, "log", LOG_FILE, "log file to which secrets are written")
 
-	connect(knownAddress)
-	startListener()
+	flag.Usage = Usage
+	flag.Parse()
+
+	go startListener()
+
+	peers = append(peers, knownAddress)
+
+	for {
+		// choose a random peer and try to connect to them
+		var err error
+		index := 0
+		if len(peers) > 0 {
+			index = mrand.Intn(len(peers))
+			if err != nil {
+				fmt.Println("Rand err:", err)
+				continue
+			}
+		}
+		peer := peers[index]
+		c, ok := connections[peer]
+		if ok {
+			if c.gotSecret {
+				go ping(*c.conn)
+			} else {
+				go query(*c.conn)
+			}
+		} else {
+			connect(peer)
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func connect(address string) {
@@ -76,7 +124,7 @@ func ping(c net.Conn) {
 
 func pong(c net.Conn, pingMessageId []byte) {
 	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.BigEndian, int16(port))
+	binary.Write(buf, binary.BigEndian, uint16(port))
 	buf.Write(net.ParseIP(host).To4())
 	send(c, Pong, pingMessageId, buf.Bytes())
 }
@@ -86,7 +134,7 @@ func query(c net.Conn) {
 }
 
 func reply(c net.Conn, queryMessageId []byte) {
-	send(c, Query, queryMessageId, []byte(secret))
+	send(c, Reply, queryMessageId, []byte(secret))
 }
 
 func send(c net.Conn, t MessageType, messageId []byte, payload []byte) {
@@ -124,38 +172,58 @@ func startListener() {
 }
 
 func handleConnection(c net.Conn) {
+	addr := c.RemoteAddr().String()
+	connections[addr] = &Conn{&c, false}
+
+	b := make([]byte, 2048)
 	for {
-		b := make([]byte, 2048)
 		n, err := c.Read(b)
 
 		if err != nil {
 			fmt.Println("Read err:", err)
+			delete(connections, addr)
+			break
 		}
 
 		if n == 0 {
 			continue
 		}
 
-		t := MessageType(b[16])
-		msgId := b[:16]
-		fmt.Printf("RECV %s %s - % x\n", t, c.RemoteAddr(), b[:n])
+		processMessage(c, b[:n])
+	}
+}
 
-		switch t {
-		case Ping:
-			go pong(c, msgId)
-		case Query:
-			go reply(c, msgId)
-		case Pong:
-			go processPong(c, b)
-		case Reply:
-			go processReply(c, b)
-		}
+func getKey(msgId []byte, mt MessageType) string {
+	return fmt.Sprint(msgId, "-", mt)
+}
+
+func processMessage(c net.Conn, b []byte) {
+	t := MessageType(b[16])
+	msgId := b[:16]
+	fmt.Printf("RECV %s %s - % x\n", t, c.RemoteAddr(), b)
+
+	key := getKey(msgId, t)
+	if _, found := msgCache.Get(key); found {
+		return
+	}
+
+	msgCache.Set(key, true, 0)
+
+	switch t {
+	case Ping:
+		go pong(c, msgId)
+	case Query:
+		go reply(c, msgId)
+	case Pong:
+		go processPong(c, b)
+	case Reply:
+		go processReply(c, b)
 	}
 }
 
 func processPong(c net.Conn, b []byte) {
 	buf := bytes.NewBuffer(b[24:])
-	var port int16
+	var port uint16
 	err := binary.Read(buf, binary.BigEndian, &port)
 	if err != nil {
 		fmt.Println("Could not read port from Pong message: ", err)
@@ -168,11 +236,10 @@ func processPong(c net.Conn, b []byte) {
 		fmt.Println("Could not read ip from Pong message: ", err)
 		return
 	}
-
 }
 
 func processReply(c net.Conn, b []byte) {
-	buf := bytes.NewBuffer(b[20:])
+	buf := bytes.NewBuffer(b[19:])
 	var size int32
 	err := binary.Read(buf, binary.BigEndian, &size)
 	if err != nil {
@@ -180,19 +247,42 @@ func processReply(c net.Conn, b []byte) {
 		return
 	}
 
+	var port uint16
+	err = binary.Read(buf, binary.BigEndian, &port)
+	if err != nil {
+		fmt.Println("Could not read port from Pong message: ", err)
+		return
+	}
+
+	ipBytes := make([]byte, 4)
+	n, err := buf.Read(ipBytes)
+	if n != 4 || err != nil {
+		fmt.Println("Could not read ip from Pong message: ", err)
+		return
+	}
+
+	size = size - 6
 	secretTextBytes := make([]byte, size)
-	n, err := buf.Read(secretTextBytes)
+	n, err = buf.Read(secretTextBytes)
 	if n != int(size) || err != nil {
+		fmt.Println("n", n, "size", size)
 		fmt.Println("Could not read secret text from Reply message: ", err)
 		return
 	}
 
-	log := fmt.Sprint("Address", c.RemoteAddr().String(), "sent secret text", string(secretTextBytes), "\n")
+	log := fmt.Sprintf("Address %v:%v sent secret text \"%v\"\n", net.IP(ipBytes).String(), port, string(secretTextBytes))
 	fmt.Print(log)
-	file, _ := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE, 0666)
-	file.WriteString(log)
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("Could not open file: ", err)
+	}
+	defer file.Close()
+
+	n, err = file.WriteString(log)
+	if n != len(log) || err != nil {
+		fmt.Println("WriteString error to log: ", err)
+	}
 	file.Sync()
-	file.Close()
 }
 
 func genId() (messageId []byte) {
